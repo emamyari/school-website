8247122c0ba70a4f8ef4d4e9db010d3cc6163b43
Mohammad Mahdi Bigdeli <mohammadmahdi.bigdeli@yahoo.com>
2022-06-27 23:05:29 +0430

    Transfer del, inc and dec functions from Product to Products

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -5,33 +5,26 @@ import "../components.css"
 class Counter extends Component {
     state = { count: this.props.number }
 
-    HandleInc = () => {
-        var copy = this.state.count
-        var set_count = copy >= 10 ? this.state.count : this.state.count + 1
-        this.setState({ count: set_count })
-    }
-
-    HandleDec = () => {
-        var copy = this.state.count
-        var set_count = copy <= 0 ? this.state.count : this.state.count - 1
-        this.setState({ count: set_count })
-    }
+    
 
     render() {
         return (<div>
             <p className="Title">{this.props.name}</p>
 
-            <button onClick={this.HandleDec} className="btn btn-warning Dec" ><svg style={{ textAlign: "senter" }} width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+            <button onClick={() => this.props.onDec(this.props.id)} className="btn btn-warning Dec" ><svg style={{ textAlign: "senter" }} width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z" />
             </svg></button>
 
             <span className={"btn btn-info Count"}>{this.state.count}</span>
 
-            <button onClick={this.HandleInc} className="btn btn-warning Inc"><svg style={{ textAlign: "senter" }} width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+            <button onClick={() => this.props.onInc(this.props.id)} className="btn btn-warning Inc"><svg style={{ textAlign: "senter" }} width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z" />
+            </svg></button>
+
+            <button onClick={() => this.props.onDelete(this.props.id)} className="btn btn-warning Del"><svg width="17" height="17" fill="currentColor" viewBox="0 0 16 16">
+              <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
             </svg></button>
         </div>);
     }
 }
 export default Counter;
-function sas() {}
