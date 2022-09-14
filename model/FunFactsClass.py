adaa21c21adaaf0a62514cde00a9a352e4c7a1a2
Bardia hasani <108074114+Bardiaq@users.noreply.github.com>
2022-09-14 12:21:49 -0700

    Delete model directory

--- model/FunFactsClass.py
+++
@@ -1,5 +0,0 @@
-class funfact():
-    def __int__(self,id,counterNumber,counterText):
-        self.id=id
-        self.counterNumber=counterNumber
-        self.counterText=counterText
--- model/students.py
+++
@@ -1,6 +0,0 @@
-class studen():
-    def __int__(self,name,family,nc,schoolname):
-        self.name=name
-        self.family=family
-        self.nc=nc
-        self.schoolname=schoolname
