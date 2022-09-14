95d16b3830a398747e882285fe3e111ca01263ab
Bardia hasani <108074114+Bardiaq@users.noreply.github.com>
2022-09-14 12:22:01 -0700

    Delete djangoProject2 directory

--- djangoProject2/dao/sqlbase.py
+++
@@ -1,9 +0,0 @@
-import pyodbc
-
-class sqlbase():
-    def getConnection(self):
-        connection=pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=185.83.183.254,11201;DATABASE=school;UID=sa;PWD=111@a')
-        cursor=connection.cursor()
-        return connection,cursor
-
-
