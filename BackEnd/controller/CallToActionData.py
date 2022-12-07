c88e6eb9db53b9fcad308b857732d71599604abd
Mohammad Mahdi Bigdeli <mohammadmahdi.bigdeli@yahoo.com>
2022-12-07 22:07:52 +0330

    delet CallToAction.py from dao folder

--- BackEnd/controller/CallToActionData.py
+++ BackEnd/controller/CallToActionData.py
@@ -1,9 +1,16 @@
 import json
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
-from dao.CallToAction import DAO
+from dao.Database import ConnectDb
+from model.CallToAction import CallToAction
+
 
 @api_view(['''GET'''])
 def CallToActionData(request):
-    data = DAO()
-    return HttpResponse(json.dumps(data), content_type='application/json')
+    rows = ConnectDb('''CallToAction''')
+    li = []
+    for row in rows:
+        data = CallToAction(row[1], row[2], row[3], row[4])
+        data = data.__dict__
+        li.append(data)
+    return HttpResponse(json.dumps(li), content_type='application/json')
--- BackEnd/controller/testimonia.py
+++ BackEnd/controller/testimonia.py
@@ -6,8 +6,6 @@ def DAO():
     rows = ConnectDb('''testimonial''')
     li = []
     for row in rows:
-        a = testimonial(row[0], row[1], row[2],row[3],row[4])
+        a = testimonial(row[0], row[1], row[2], row[3], row[4])
         li.append(a.__dict__)
-        # data = a.__dict__
-
     return li
