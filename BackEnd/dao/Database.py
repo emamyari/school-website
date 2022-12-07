9cfdbcd9af955893d84c113bff021a7fce1dddb9
Mohammad Mahdi Bigdeli <mohammadmahdi.bigdeli@yahoo.com>
2022-12-07 17:23:48 +0330

    creat MicrosoftAccese DataBase

--- BackEnd/dao/Database.py
+++ BackEnd/dao/Database.py
@@ -1,15 +1,27 @@
 import pyodbc
+from dao.Model import Products
+import os
 
-
-def ConnectDb(TableName):
-    connection = pyodbc.connect(
-        'DRIVER={ODBC Driver 17 for SQL server};'
-        'SERVER=.;'
-        'DATABASE=school;'
-        'UID=sa;'
-        'PWD=111;')
-    cursor = connection.cursor()
-    cursor.execute('''SELECT * FROM ''' + str(TableName))
-    rows = cursor.fetchall()
+def ConnectDB(TableName):
+    conn = pyodbc.connect(
+        r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=" + str(os.getcwd()) + "\DB.accdb;")
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM ' + str(TableName))
+    json = cursor.fetchall()
     connection.close()
-    return rows
+    return json
+# import pyodbc
+#
+#
+# def ConnectDb(TableName):
+#     connection = pyodbc.connect(
+#         'DRIVER={ODBC Driver 17 for SQL server};'
+#         'SERVER=.;'
+#         'DATABASE=school;'
+#         'UID=sa;'
+#         'PWD=111;')
+#     cursor = connection.cursor()
+#     cursor.execute('''SELECT * FROM ''' + str(TableName))
+#     rows = cursor.fetchall()
+#     connection.close()
+#     return rows
